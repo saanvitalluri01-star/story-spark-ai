16e1ad359a8c8848bb993880b883714b76447613
Toufiq Amin <toufiqsuri2006@gmail.com>
2026-06-12 18:08:14 +0530

    fix(ui): correct unbalanced closing tags at end of file

--- frontend/src/components/home/writer_feedback/ReviewForm.tsx
+++ frontend/src/components/home/writer_feedback/ReviewForm.tsx
@@ -300,12 +300,9 @@ const ReviewForm = () => {
                 "Share Review ✨"
               )}
             </button>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
-  );
     </div>
   );
 };
